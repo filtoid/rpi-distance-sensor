ed7cec29b225b6b63680d3bbc9f1cf8a09e02098
Filtoid <philjeffes@hotmail.com>
2017-10-13 12:25:43 +0100

    Update measure_distance.py

--- measure_distance.py
+++ measure_distance.py
@@ -13,21 +13,25 @@ GPIO.output(TRIG, False)
 print("Letting sensor settle")
 time.sleep(2)
 
-GPIO.output(TRIG, True)
+def measure_distance():
+	GPIO.output(TRIG, True)
 
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
+	time.sleep(0.00001)
+	GPIO.output(TRIG, False)
 
-while GPIO.input(ECHO)==0:
-	pulse_start = time.time()
+	while GPIO.input(ECHO)==0:
+		pulse_start = time.time()
 
-while GPIO.input(ECHO)==1:
-	pulse_end = time.time()
+	while GPIO.input(ECHO)==1:
+		pulse_end = time.time()
 
-pulse_dur = pulse_end - pulse_start
-distance = pulse_dur * 17150
-distance = round(distance, 2)
+	pulse_dur = pulse_end - pulse_start
+	distance = pulse_dur * 17150
+	distance = round(distance, 2)
+	
+	print(str(distance) + " centimetres")
+	return distance
 
-print(str(distance) + " centimetres")
+measure_distance()
 
 GPIO.cleanup()
